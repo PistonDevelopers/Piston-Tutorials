b76ff46fc1e8ca2910d5a3a0c2bafb932ad768b9
Ty Overby <ty@pre-alpha.com>
2014-07-20 15:48:02 -0700

    added more comments, moved the source file

--- src/game.rs
+++ src/game.rs
@@ -22,18 +22,18 @@ use graphics::{
 };
 
 pub struct App {
-    gl: Gl,       // OpenGL drawing backend
-    rotation: f64 // Rotation for the square
+    gl: Gl,       // OpenGL drawing backend.
+    rotation: f64 // Rotation for the square.
 }
 
 impl Game for App {
     fn render(&mut self, args: &RenderArgs) {
-        // Set up a context to draw into
+        // Set up a context to draw into.
         let context = &Context::abs(args.width as f64, args.height as f64);
-        // Clear the screen
+        // Clear the screen.
         context.rgba(0.0,1.0,0.0,1.0).draw(&mut self.gl);
 
-        // Draw a box rotating around the middle of the screen
+        // Draw a box rotating around the middle of the screen.
         context
             .trans((args.width / 2) as f64, (args.height / 2) as f64)
             .rot_deg(self.rotation)
@@ -42,13 +42,13 @@ impl Game for App {
             .trans(-25.0, -25.0)
             .draw(&mut self.gl);
 
-        // Rotate by one degree
+        // Rotate by one degree.
         self.rotation += 1.0;
     }
 }
 
 fn main() {
-    // Create a GLFW window
+    // Create a GLFW window.
     let mut window = GameWindowGLFW::new(
         GameWindowSettings {
             title: "Hello Piston".to_string(),
@@ -58,12 +58,13 @@ fn main() {
         }
     );
 
+    // Some settings for how the game should be run.
     let game_iter_settings = GameIteratorSettings {
         updates_per_second: 60,
         max_frames_per_second: 60
     };
 
-    // Create a new game and run it
+    // Create a new game and run it.
     let mut app = App { gl: Gl::new(), rotation: 0.0 };
     app.run(&mut window, &game_iter_settings);
 }
