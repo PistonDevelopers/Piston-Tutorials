b3b1b052eb03baba280bd04c7471b1a92201064e
Ty Overby <ty@pre-alpha.com>
2014-07-19 22:28:06 -0700

    first commit

---
+++ src/hello.rs
@@ -0,0 +1,69 @@
+extern crate graphics;
+extern crate piston;
+extern crate glfw_game_window;
+extern crate opengl_graphics;
+
+use glfw_game_window::GameWindowGLFW;
+use opengl_graphics::Gl;
+
+use piston::{
+    Game,
+    GameWindowSettings,
+    GameIteratorSettings,
+    RenderArgs
+};
+
+use graphics::{
+    Context,
+    AddRectangle,
+    AddColor,
+    Draw,
+    RelativeTransform2d,
+};
+
+pub struct App {
+    gl: Gl,       // OpenGL drawing backend
+    rotation: f64 // Rotation for the square
+}
+
+impl Game for App {
+    fn render(&mut self, args: &RenderArgs) {
+        // Set up a context to draw into
+        let context = &Context::abs(args.width as f64, args.height as f64);
+        // Clear the screen
+        context.rgba(0.0,1.0,0.0,1.0).draw(&mut self.gl);
+
+        // Draw a box rotating around the middle of the screen
+        context
+            .trans((args.width / 2) as f64, (args.height / 2) as f64)
+            .rot_deg(self.rotation)
+            .rect(0.0, 0.0, 50.0, 50.0)
+            .rgba(1.0, 0.0, 0.0,1.0)
+            .trans(-25.0, -25.0)
+            .draw(&mut self.gl);
+
+        // Rotate by one degree
+        self.rotation += 1.0;
+    }
+}
+
+fn main() {
+    // Create a GLFW window
+    let mut window = GameWindowGLFW::new(
+        GameWindowSettings {
+            title: "Hello Piston".to_string(),
+            size: [800, 800],
+            fullscreen: false,
+            exit_on_esc: true
+        }
+    );
+
+    let game_iter_settings = GameIteratorSettings {
+        updates_per_second: 60,
+        max_frames_per_second: 60
+    };
+
+    // Create a new game and run it
+    let mut app = App { gl: Gl::new(), rotation: 0.0 };
+    app.run(&mut window, &game_iter_settings);
+}
